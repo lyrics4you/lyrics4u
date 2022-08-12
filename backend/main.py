0eea3a724dbf9044f837228f70f181ad6e1ab250
Han Seong-Min <tmpks3@gmail.com>
2022-08-13 01:03:08 +0900

    Update: get route for scrapping songs info

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, HTTPException, status
+from utils.scrapper import get_song_list
 
 app = FastAPI()
 
 
-@app.get("/")
-async def get_songs_info():
-    return {"result": "Hello, World!"}
+@app.get("/songs")
+async def get_songs_info(query: str):
+    song_info = get_song_list(query)
+    if not song_info:
+        raise HTTPException(status_code=404, detail="검색결과가 없습니다.")
+    return {"result": song_info}
